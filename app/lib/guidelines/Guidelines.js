0f56597b61d9db84d7276495ea4b2f4cceece680
MaximilianKoenig <maximilian.koenig@student.hpi.uni-potsdam.de>
2024-02-21 15:47:21 +0100

    🔥 Remove obsolete guidelines

--- app/lib/guidelines/Guidelines.js
+++ app/lib/guidelines/Guidelines.js
@@ -27,24 +27,6 @@ SEVERITY.filter = function(lambda) {
 
 export default [
     {
-       title : 'GS2: Include all relevant data objects in the goal',
-       id : 'GS2',
-       getViolations(mediator) {
-           const hook = mediator.goalStateModelerHook;
-           const literals = hook.modeler.getLiterals();
-           if (literals && literals.length === 0) {
-               return [{
-                   element : hook.getRootObject(),
-                   message : 'Please include at least one data object configuration as literal in the goal state.'
-               }];
-           } else {
-               return [];
-           }
-       },
-       severity : SEVERITY.ERROR,
-       link : 'https://github.com/bptlab/fCM-design-support/wiki/Goal-State#gs2---include-all-relevant-data-objects-in-the-goal-state'
-    },
-    {
         title: 'F9: Do not use gateways at the beginning of a fragment',
         id: 'F9',
         getViolations(mediator) {
@@ -80,7 +62,7 @@ export default [
         link: 'https://github.com/bptlab/fCM-design-support/wiki/Fragments#f3---use-at-least-one-activity-for-a-fragment'
     },
     {
-        title: 'F11: Label notations elements',
+        title: 'F11: Label notation elements',
         id: 'F11',
         getViolations(mediator) {
             const elements = mediator.fragmentModelerHook.modeler.get('elementRegistry').filter(element =>
@@ -148,81 +130,6 @@ export default [
         },
         severity: SEVERITY.WARNING,
         link: 'https://github.com/bptlab/fCM-design-support/wiki/Fragments#f4---use-data-objects-to-model-pre--and-postconditions'
-    },
-    {
-        title: 'F6A: Use at least one start event',
-        id: 'F6A',
-        getViolations(mediator) {
-            const hook = mediator.fragmentModelerHook;
-            const startEvents = hook.modeler.get('elementRegistry').filter(element => is(element, 'bpmn:StartEvent') && element.type !== 'label');
-            if (startEvents.length === 0) {
-                return [{
-                    element: hook.getRootObject(),
-                    message: 'Please use at least one start event in the fragment model.'
-                }];
-            } else {
-                return [];
-            }
-        },
-        severity: SEVERITY.ERROR,
-        link: 'https://github.com/bptlab/fCM-design-support/wiki/Fragments#f6---use-start-events-only-in-initial-fragments'
-    },
-    {
-        title: 'F6B: Use multiple start events carefully',
-        id: 'F6B',
-        getViolations(mediator) {
-            const hook = mediator.fragmentModelerHook;
-            const startEvents = hook.modeler.get('elementRegistry').filter(element => is(element, 'bpmn:StartEvent') && element.type !== 'label');
-            if (startEvents.length > 1) {
-                return startEvents.map(element => ({
-                    element: element.businessObject,
-                    message: 'Your process has multiple start events. Please ensure that this is intended.'
-                }));
-            } else {
-                return [];
-            }
-        },
-        severity: SEVERITY.INFORMATION,
-        link: 'https://github.com/bptlab/fCM-design-support/wiki/Fragments#f6---use-start-events-only-in-initial-fragments'
-    },
-    {
-        title: 'F6C: Start fragment does not create case class',
-        id: 'F6C',
-        getViolations(mediator) {
-            const dataModeler = mediator.dataModelerHook.modeler;
-            const fragmentModeler = mediator.fragmentModelerHook.modeler;
-            const startEvents = fragmentModeler.get('elementRegistry').filter(element => is(element, 'bpmn:StartEvent') && element.type !== 'label');
-            const caseClasses = dataModeler.get('elementRegistry')
-                .filter(element => is(element, 'od:Class'))
-                .map(element => element.businessObject)
-                .filter(clazz => clazz.caseClass);
-            if (caseClasses.length === 0) {return [];}
-
-            return startEvents.flatMap(startEvent => {
-                const connectedElements = getConnectedElements(startEvent);
-
-                // get according connected dataobjects
-                const createdClasses = connectedElements.filter(element => is(element, 'bpmn:DataObjectReference')).map(element => element.businessObject.dataclass);
-                const caseClassConnected = createdClasses.some(createdClass => caseClasses.includes(createdClass));
-
-                if (!caseClassConnected) {
-                    return [{
-                        element: startEvent.businessObject,
-                        message: 'Please make one of the nodes of this start fragment create an object of one of the case classes: ' + caseClasses.map(clazz => '"' + clazz.name + '"').join(', '),
-                        quickFixes : caseClasses.flatMap(caseClass => connectedElements.filter(element => is(element, 'bpmn:Activity') || is(element, 'bpmn:StartEvent')).map(element => (
-                            {
-                                label : 'Add data object reference that writes case class "' + caseClass.name + '" to node "' + element.businessObject.name + '"',
-                                action : (event) => fragmentModeler.startDoCreation(event, element, caseClass)
-                            }
-                        )))
-                    }];
-                } else {
-                    return [];
-                }
-            });
-        },
-        severity: SEVERITY.ERROR,
-        link: 'https://github.com/bptlab/fCM-design-support/wiki/Fragments#f6---use-start-events-only-in-initial-fragments'
     },    
     {
         title : 'Have each fragment state transition in olc',
@@ -359,65 +266,5 @@ export default [
         },
         severity: SEVERITY.ERROR,
         link: 'https://github.com/bptlab/fCM-design-support/wiki/Consistency#c5---provide-context-data-objects-from-data-model-on-data-object-creation-in-fragments'
-    },
-    {
-        title : 'Specify a Case Class.',
-        id : 'D2',
-        getViolations(mediator) {
-            const hook = mediator.dataModelerHook;
-            const dataModeler = hook.modeler;
-            const clazzes = dataModeler.get('elementRegistry').getAll().filter(element => is(element, 'od:Class'));
-            
-            const caseClasses = dataModeler.get('elementRegistry')
-                .filter(element => is(element, 'od:Class'))
-                .map(element => element.businessObject)
-                .filter(clazz => clazz.caseClass);
-            
-            if (!caseClasses.length) {
-                return [{
-                    element : hook.getRootObject(),
-                    message : 'Please specify a case class.',
-                    quickFixes : clazzes.map(clazz => (
-                            {
-                                label : 'Make class "' + clazz.businessObject.name + '" a case class',
-                                action : (action) => dataModeler.updateProperty(clazz, {caseClass: true})
-                            }
-                    ))
-                }];
-            } else {
-                return [];
-            }
-        },
-        severity : SEVERITY.ERROR,
-        link : 'https://github.com/bptlab/fCM-design-support/wiki/Data-Model#d2---specify-a-case-class'
-    },
-    {
-        title : 'Connect the Case Class to Every Other Class.',
-        id : 'D3',
-        getViolations(mediator) {
-            const dataModeler = mediator.dataModelerHook.modeler;
-            const classDependents = getClassDependents(mediator);
-            const clazzes = dataModeler.get('elementRegistry')
-                .filter(element => is(element, 'od:Class'))
-                .map(element => element.businessObject);
-            
-            const caseClasses = clazzes.filter(clazz => clazz.caseClass);
-
-            if (!caseClasses.length) {
-                return [];
-            }
-            
-            const connectedWithCaseClass = getConnectedByExistentialAssociation(caseClasses[0], classDependents);
-            const notConnectedClasses = clazzes.filter(clazz => connectedWithCaseClass.indexOf(clazz) === -1);
-
-            return notConnectedClasses.map(clazz => ({
-                element: clazz,
-                message: 'Please connect class "' + clazz.name + '" to the case class via a path of existential associations.'
-            }
-            ))
-
-        },
-        severity : SEVERITY.ERROR,
-        link : 'https://github.com/bptlab/fCM-design-support/wiki/Data-Model#d3---connect-the-case-class-to-every-other-class'
     },
 ]
